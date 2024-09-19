a8e3d150b319a9e86da6326c6dfb8f59cc798730
Valérie <valleeriee@yahoo.fr>
2024-09-19 16:57:51 +0200

    Correction pour soutenance du fichier login.js

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,12 +1,3 @@
-// Compte de test pour Sophie Bluel
-// email: sophie.bluel@test.tld
-// password: S0phie 
-
-const compteId = {
-    email: "sophie.bluel@test.tld",
-    password: "S0phie"
-}
-
 // Envoi des valeurs du formulaire
 function ajoutListenerLogin() {
     const formulaireLogin = document.getElementById("formulaire-login");
@@ -21,7 +12,7 @@ function ajoutListenerLogin() {
         }
         const chargeUtile = JSON.stringify(loginId)
 
-        if ((loginId.email === compteId.email) && (loginId.password === compteId.password)) {
+        try {
             let response = await fetch("http://localhost:5678/api/users/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -31,7 +22,7 @@ function ajoutListenerLogin() {
             let token = result['token'];
             localStorage.setItem("Token", token)
             window.location = "index.html"
-        } else {
+        } catch {
             const erreur = document.createElement("p")
             erreur.innerText = "Erreur dans l’identifiant ou le mot de passe"
             erreur.classList.add("txt-error")
